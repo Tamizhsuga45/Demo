aa48c5b9a1ab9ebd94e71f0d0a83b079260e84a5
Tamizhanbu <113338664+Tamizhsuga45@users.noreply.github.com>
2023-11-03 09:01:44 +0530

    Add files via upload
    Day3program

---
+++ multilevelinherteritance/Main.java
@@ -0,0 +1,15 @@
+package multilevelinherteritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Circle c1 = new Circle();
+       
+        c1.area(5,8);
+        c1.area(5);
+        c1.Color();
+	}
+
+}
+
---
+++ multilevelinherteritance/Shape.java
@@ -0,0 +1,8 @@
+package multilevelinherteritance;
+
+public class Shape {
+    public void Color() {
+   	 System.out.println("The color is  blue");
+    }
+
+}
