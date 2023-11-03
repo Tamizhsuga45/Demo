db15f3a21ae5cc954a284700b8392b0925bd960f
Tamizhanbu <113338664+Tamizhsuga45@users.noreply.github.com>
2023-11-03 08:59:04 +0530

    Delete multipleinheritance directory

--- multipleinheritance/Circle.java
+++
@@ -1,8 +0,0 @@
-package multipleinheritance;
-
-public class Circle extends Triangle {
-	public void area(int r){
-        double area = 3.14*r*r;
-        System.out.println("The Circle Area is " + area);
-}
-}
